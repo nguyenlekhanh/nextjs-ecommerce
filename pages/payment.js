2a59e9a936c6c93ef79a2a15f9b19687d885ee1e
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2021-10-25 20:45:56 -0500

    remove another payment

--- pages/payment.js
+++ pages/payment.js
@@ -64,16 +64,6 @@ export default function Payment() {
                   value="PayPal"
                   control={<Radio />}
                 ></FormControlLabel>
-                <FormControlLabel
-                  label="Stripe"
-                  value="Stripe"
-                  control={<Radio />}
-                ></FormControlLabel>
-                <FormControlLabel
-                  label="Cash"
-                  value="Cash"
-                  control={<Radio />}
-                ></FormControlLabel>
               </RadioGroup>
             </FormControl>
           </ListItem>
